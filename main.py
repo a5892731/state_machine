ef60d5107f845391d0cd9362a355279ea8a15e64
jakubz <a5892731@o2.pl>
2022-05-23 08:57:42 +0200

    loop time check

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 version 2.0
 author: a5892731
 date: 2021-03-17
-update: 2022-05-21
+update: 2022-05-23
 
 description:
 this is a simple state machine
@@ -14,9 +14,9 @@ required modules:
 
 
 '''
+from time import time
 
 from resources.state_machine.state_loader import StateLoader
-
 
 #--------------------------------------------------------------------------------------- <<<< MAIN MENU
 
@@ -24,6 +24,12 @@ from resources.state_machine.state_loader import StateLoader
 
 if __name__ == "__main__":
     device = StateLoader()
-
     while True:
-        device.on_event('device_locked')
+        start_time = time()
+
+
+        device.on_event('device_locked') # call the state machine events
+
+
+        loop_time = time() - start_time
+        print(">>> main loop time = {}".format(loop_time))
